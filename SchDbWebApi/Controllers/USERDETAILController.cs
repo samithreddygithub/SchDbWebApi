fe96775a7d0472c7428e42e2eff5d887be0b2c55
samithreddygithub <samithreddygithub@gmail.com>
2017-02-11 07:02:14 +0530

    controller details

---
+++ SchDbWebApi/Controllers/USERDETAILController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SchDbWebApi.Controllers
+{
+    public class USERDETAILController : ApiController
+    {
+        string connectionstring = ConfigurationManager.ConnectionStrings["SCHDBWEBAPI"].ConnectionString;
+
+
+
+    }
+}
