f81ed4bccbc497a18bd790b674d23cce494a02d2
samithreddygithub <samithreddygithub@gmail.com>
2017-02-11 06:54:11 +0530

    common and userbinding

---
+++ SchDbWebApi/Models/COMMONMODEL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchDbWebApi.Models
+{
+    public class COMMONMODEL
+    {
+    }
+
+    public class CLASSSECTIONMODEL
+    {
+        public int csid { get; set; }
+        public string csclassname { get; set; }
+        public string cssectionname { get; set; }
+        public string cscreateddate { get; set; }
+        public string csmodifieddate { get; set; }
+        public string csstatus { get; set; }
+        public int csoid { get; set; }
+    }
+
+    public class CLASSSUBJECTMODEL
+    {
+        public int csubid { get; set; }
+        public int csubclasssectionid { get; set; }
+        public string csubsubjectname { get; set; }
+        public int csstaffnameid { get; set; }
+        public string csubcreateddate { get; set; }
+        public string csubmodifieddate { get; set; }
+        public string csubstatus { get; set; }
+        public int csuboid { get; set; }
+    }
+
+}
---
+++ SchDbWebApi/Models/USERBINDINGMODEL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchDbWebApi.Models
+{
+    public class USERBINDINGMODEL
+    {
+        public int buid { get; set; }
+        public int bustudentid { get; set; }
+        public int buparentid { get; set; }
+        public int buclasssectionid { get; set; }
+        public string bucreateddate { get; set; }
+        public string bumodifieddate { get; set; }
+        public string bustatus { get; set; }
+        public int buoid { get; set; }
+    }
+}
